306cb1617978a1022b766ab8996f0bcf96c92534
Mikew1999 <mikewalters1234@gmail.com>
2021-05-26 23:59:32 +0000

    added sign in form

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, session
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -21,6 +24,19 @@ def index():
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # checks if username exists in db
+        is_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        # if user exists flash message and return to signup page
+        if is_user:
+            flash("Username already exists")
+            return render_template("signup.html")
 
 
 if __name__ == "__main__":
