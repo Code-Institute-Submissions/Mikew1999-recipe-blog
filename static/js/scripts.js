144ad42114fef8821446faef44c445236e0415b2
Mikew1999 <mikewalters1234@gmail.com>
2021-07-04 18:08:15 +0000

    added common styles for all forms (login, signup, create recipe)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,8 +2,24 @@
 function moreIngredients() {
     // counts number of ingredients in form
     let ingredientnumber = $('.ingredient').length;
+    // adds 1 to ingredient number
     ingredientnumber += 1;
-    let ingredient = `<label class="ingredient" for="ingredient${ingredientnumber}">Ingredient ${ingredientnumber}:</label>
-    <input type="text" name="ingredient${ingredientnumber}"></input>`
+    // creates ingredient item
+    let ingredient = `<div class="mb-2"><label class="ingredient" for="ingredient${ingredientnumber}">Ingredient ${ingredientnumber}:</label>
+    <input class="form-control" type="text" name="ingredient${ingredientnumber}"></input></div>`
+    // adds new ingredient item before more ingredient button
     $(ingredient).insertBefore('#moreingredientsbutton');
+}
+
+// adds another step label and input
+function moresteps() {
+    // counts number of steps in form
+    let stepnumber = $('.step').length;
+    // adds 1 to ingredient number
+    stepnumber += 1;
+    // creates step item
+    let step = `<div class="mb-2"></div><label class="step" for="step${stepnumber}">Step ${stepnumber}:</label>
+    <input class="form-control" type="text" name="step${stepnumber}"></input></div>`
+    // adds new step item before more ingredient button
+    $(step).insertBefore('#morestepsbutton');
 }
